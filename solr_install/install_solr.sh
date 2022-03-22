ef7e0d4916571cb2fabb46ad9fe38f474c26d29f
Anil Koppula <72085415+anilkumaran@users.noreply.github.com>
2022-03-22 16:49:41 +0530

    Update install_solr.sh

--- solr_install/install_solr.sh
+++ solr_install/install_solr.sh
@@ -5,21 +5,3 @@ sudo apt install default-jdk
 wget http://www.gtlib.gatech.edu/pub/apache/lucene/solr/8.11.1/solr-8.11.1.tgz
 tar zxvf solr-8.11.1.tgz
 sudo solr-8.11.1/bin/install_solr_service.sh solr-8.11.1.tgz
-
-## CREATE CORE
-SOLR_SRC="/opt/solr"      # symlink to your solr-<version> directory
-SOLR_ROOT="/var/solr"
-SOLR_HOME="${SOLR_ROOT}/data"
-CORE=$1
-
-# Create core config set in SOLR_HOME
-cd ${SOLR_HOME}/
-mkdir -p ${CORE}/data
-cp -R ${SOLR_SRC}/server/solr/configsets/_default/conf/ ./${CORE}/ # from default conf
-#cp -R ${SOLR_SRC}/server/solr/${CORE}/conf/ ./${CORE}/
-
-# Set ownership
-chown -R solr:solr ${SOLR_HOME}
-
-# Create core
-${SOLR_SRC}/bin/solr create -c ${CORE} -force
